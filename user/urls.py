dc999a61717233c09ab171d6f1489e1b35c00514
jongsoo-P <jongsooshi@gmail.com>
2023-07-19 15:51:05 +0900

    회원가입, 로그인 기능 추가

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    
+    path("register/", views.Registration.as_view(), name='register'),
+    path("login/", views.Login.as_view(), name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm, LoginForm
 
-# Create your views here.
+
+class Registration(View):
+
+    def get(self, request):
+        # if request.user.is_authenticated:
+        #     return redirect('blog:list')
+        form = RegisterForm()
+        context = {
+            'form': form,
+            'title': 'User'
+        }
+        return render(request, 'user/user_register.html', context)
+    
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        print(form)
+        print(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('user:login')
+        return redirect('user:register')
+
+
+class Login(View):
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('blog:list')
+        form = LoginForm()
+        context = {
+            'form': form,
+            'title': 'User'
+        }
+        return render(request, 'user/user_login.html', context)
+    
+    def post(self, request):
+        if request.user.is_authenticated:
+            return redirect('blog:list')
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=email, password=password)
+            
+            if user:
+                login(request, user)
+                return redirect('blog:list')
+        context = {
+            'title': 'User'
+        }
+        return render(request, 'user/user_login.html', context)
+
